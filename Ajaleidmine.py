76f2fda130bb2b6e17d38f9a52cd25d00386bb4b
Gert-Andry Kääramees <gertandry@gmail.com>
2023-07-25 23:43:56 +0300

    update code

--- Ajaleidmine.py
+++ Ajaleidmine.py
@@ -10,44 +10,54 @@ import winsound
 
 global saadaEmail
 
-idpin = "XXXX"
+emailserver = "smtp.sendgrid.net"
 emailto = ["emailto@gmail.com"]
 emailfrom = "emailfrom@gmail.com"
+emailusername = "apikey"
 emailpass = "emailfrom_pass"
 #Hetkel registreeritud soiduaeg
-currentDate = "25.09.2019"
+currentDate = "03.08.2023"
 
 saadaEmail = True
 fox = webdriver.Chrome()
 fox.get("https://eteenindus.mnt.ee/main.jsf")
-input("sisse logitud? ")
+input("sisse logitud? (vajuta enter kui jah)")
 
 while True:
     try:
-        time.sleep(5)
+        # Pealeht
         element = fox.find_element(By.ID, "j_idt140:j_idt159")
         element.click()
-        time.sleep(2)
+
+        # Juhi vaade
+        time.sleep(60)
+        element = fox.find_elements(By.XPATH, "//*[text()='Muuda valikut »']")[0]
+        element.click()
+
+        # Soiduaegade vaade
+        time.sleep(25)
         element = fox.find_element(By.XPATH, "//*[text()='Kus saab kõige kiiremini eksamile?']")
         element.click()
-        time.sleep(3)
-        element = fox.find_element(By.ID, "varaseimadEksamiajadForm").find_element(By.TAG_NAME, "ul")
-        print(element)
-        varaseimAeg = element.find_element(By.XPATH, ".//strong").text
+
+        # Kiireimad soiduajad modal
+        time.sleep(2)
+        varaseimAegItem = fox.find_element(By.XPATH, "(//ul)[last()]/li[1]")
+        varaseimAeg = varaseimAegItem.find_element(By.XPATH, ".//strong").text
         print(datetime.now().time())
-        print(element.text)
+        print(varaseimAegItem.text)
         print(varaseimAeg)
 
         if varaseimAeg != currentDate and saadaEmail == True:
             #Saadab emaili et uus aeg on
             print("Email saadetud")
             winsound.PlaySound("found.wav", winsound.SND_FILENAME)
-            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-            server.login(emailfrom, emailpass)
+            server = smtplib.SMTP_SSL(emailserver, 465)
+            server.login(emailusername, emailpass)
             emailText = """To: {}
+            From: {}
             Subject: {}\n
             Uus soiduaeg tuli:\n{}
-            """.format(emailto, varaseimAeg, ascii(element.text))
+            """.format(emailto, emailfrom, varaseimAeg, ascii(varaseimAegItem.text))
             server.sendmail(
             emailfrom, 
             emailto, 
@@ -57,18 +67,24 @@ while True:
             saadaEmail = False
         elif varaseimAeg == currentDate and saadaEmail == False:
             #Saadab emaili, et koht võeti ära
-            print("Email saadetud")
-            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-            server.login(emailfrom, emailpass)
+            print("Email saadetud - soiduaeg voeti ara")
+            server = smtplib.SMTP_SSL(emailserver, 465)
+            server.login(emailusername, emailpass)
+            emailText = """To: {}
+            From: {}
+            Subject: Soiduaeg voeti ara\n
+            """.format(emailto, emailfrom)
             server.sendmail(
             emailfrom, 
             emailto, 
-            "Soiduaeg voeti ara")
+            emailText)
             server.quit()
+            
             saadaEmail = True
-        time.sleep(55)
+        time.sleep(60)
+        print()
         fox.get("https://eteenindus.mnt.ee/main.jsf")
-    except:
+    except Exception as e:
         print("crash")
         fox.get("https://eteenindus.mnt.ee/main.jsf")
         time.sleep(5)
